bf274620c747d0a9142d43dd984e8c7cbf1b793d
yung-coder <chandel.sanjeev5@gmail.com>
2023-04-19 23:24:05 +0530

    controllers

--- controllers/menuController.go
+++ controllers/menuController.go
@@ -1,10 +1,33 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"backend/database"
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var menuColletion *mongo.Collection = database.OpenCollection(database.Client, "menu")
 
 func GetMenus() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		result, err := menuColletion.Find(context.TODO(), bson.M{})
+		defer cancel()
+		if err != nil {
+			c.JSONP(http.StatusInternalServerError, gin.H{"error": "error occcured while menu items"})
+		}
 
+		var allMenus []bson.M
+
+		if err = result.All(ctx, &allMenus); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allMenus)
 	}
 }
 
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/gin-gonic/gin v1.9.0
+	github.com/go-playground/validator/v10 v10.11.2
 	go.mongodb.org/mongo-driver v1.11.4
 	golang.org/x/net v0.7.0
 )
@@ -14,7 +15,6 @@ require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.11.2 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
